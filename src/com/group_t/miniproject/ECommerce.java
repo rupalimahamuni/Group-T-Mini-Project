406cc0a0094516fb4bde11665e694a3f7f412f2a
Rupali <rupalimahamuni2596@gmail.com>
2023-01-24 21:35:15 +0530

    ECommerce interface created

---
+++ src/com/group_t/miniproject/ECommerce.java
@@ -0,0 +1,12 @@
+package com.group_t.miniproject;
+
+public interface ECommerce {
+
+	public void userRegistration();
+	
+	public void userLogin();
+	
+	public void signUp();
+	
+	public void adminLogin();
+}
